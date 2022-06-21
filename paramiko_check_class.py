7e15c67d3d2350bc88a5b7bfe15fd3566a152cf0
lc562233818 <33318891+lc562233818@users.noreply.github.com>
2022-06-21 11:26:27 +0800

    Update paramiko_check_class.py

--- paramiko_check_class.py
+++ paramiko_check_class.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+from gc import collect
+from http import client
+import sys
+from time import sleep
+import paramiko
+import traceback
+import json
+import pymysql
+
 class Connection:
     def __init__(self, host, port, user, password):
         self.host = host
@@ -27,3 +37,43 @@ class Connection:
         stdout = "".join(chan.makefile('r'))
         stderr = "".join(chan.makefile_stderr('r'))
         return chan.recv_exit_status(), stdout, stderr
+    
+    
+    
+   class Mysql_Conn:
+    def __init__(self, hosts, password, user, database):
+        self.host = hosts
+        self.password = password
+        self.user = user
+        self.database = database
+        self.charset='utf-8'
+    def insert_conn(self, sql):
+        conn = pymysql.connect(
+            host=self.host,
+            user=self.user,
+            passwd=self.password,
+            database=self.database,
+            charset=self.charset
+        )
+        cursor = conn.cursor()
+        sql = sql
+        try:
+            cursor.execute(sql)
+            conn.commit()
+        except:
+            conn.rollback()
+        cursor.close()
+        conn.close()
+    def select_conn(self, sql):
+        conn = pymysql.connect(
+            host=self.host,
+            user=self.user,
+            passwd=self.password,
+            database=self.database,
+            charset=self.charset
+        )
+        cursor = conn.cursor()
+        sql = sql
+        cursor.execute(sql)
+        cursor.close()
+        conn.close()
